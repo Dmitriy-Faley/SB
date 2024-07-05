f4348feb0337e9805eaf49c33255ddadb0b4dbcc
Dmitriy Faley <dima.faley.98@mail.ru>
2024-07-05 17:21:24 +0300

    Ititial commit

---
+++ src/js/main.js
@@ -0,0 +1,158 @@
+//import Swiper from 'swiper';
+
+
+$(document).ready(function () {
+    $('.menu-btn').click(function (e) {
+        e.preventDefault();
+        //$(this).toggleClass('menu-btn_active');
+        //$(this).toggleClass("active");
+        $('.mobile-menu').toggleClass('mobile-menu_active');
+        $('.page-content').toggleClass('page-content_active');
+        $('.overlay').toggleClass('overlay_active');
+        $("body").toggleClass("locked");
+    });
+
+    $('.close').click(function () {
+        //e.preventDefault();
+        $('.menu-btn').toggleClass('menu-btn_active');
+        $('.menu-btn').toggleClass("active");
+        $('.mobile-menu').toggleClass('mobile-menu_active');
+        $('.page-content').toggleClass('page-content_active');
+        $('.overlay').toggleClass('overlay_active');
+        $("body").toggleClass("locked");
+    });
+});
+
+
+const allRanges = document.querySelectorAll(".range-wrap");
+allRanges.forEach(wrap => {
+    const range = wrap.querySelector(".range");
+    const bubble = wrap.querySelector(".bubble");
+
+    range.addEventListener("input", () => {
+        setBubble(range, bubble);
+    });
+    setBubble(range, bubble);
+});
+
+function setBubble(range, bubble) {
+    const val = range.value;
+    const min = range.min ? range.min : 0;
+    const max = range.max ? range.max : 100;
+    const newVal = Number(((val - min) * 100) / (max - min));
+    bubble.innerHTML = val;
+
+    // Sorta magic numbers based on size of the native UI thumb
+    //bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
+}
+
+//Прикрепить резюме
+$(document).ready(function () {
+    $('input[name="file-422"]').change(function (e) {
+        var fileName = e.target.files[0].name;
+        $('.file_pretext').text(fileName);
+        $('.file_content').addClass('change-file');
+    });
+
+    $('.delete-file').click(function (e) {
+        $('.file_pretext').text('Прикрепить файл');
+        $('.file_content').removeClass('change-file');
+    });
+
+
+    $('.select').each(function () {
+        const _this = $(this),
+            selectOption = _this.find('option'),
+            selectOptionLength = selectOption.length,
+            selectedOption = selectOption.filter(':selected'),
+            duration = 450; // длительность анимации 
+
+        _this.hide();
+        _this.wrap('<div class="select"></div>');
+        $('<div>', {
+            class: 'new-select',
+            text: _this.children('option:first-child').text()
+        }).insertAfter(_this);
+
+        const selectHead = _this.next('.new-select');
+        $('<div>', {
+            class: 'new-select__list'
+        }).insertAfter(selectHead);
+
+        const selectList = selectHead.next('.new-select__list');
+        for (let i = 1; i < selectOptionLength; i++) {
+            $('<div>', {
+                    class: 'new-select__item',
+                    html: $('<span>', {
+                        text: selectOption.eq(i).text()
+                    })
+                })
+                .attr('data-value', selectOption.eq(i).val())
+                .appendTo(selectList);
+        }
+
+        const selectItem = selectList.find('.new-select__item');
+        selectList.slideUp(0);
+        selectHead.on('click', function () {
+            if (!$(this).hasClass('on')) {
+                $(this).addClass('on');
+                selectList.slideDown(duration);
+
+                selectItem.on('click', function () {
+                    let chooseItem = $(this).data('value');
+
+                    $('select').val(chooseItem).attr('selected', 'selected');
+                    selectHead.text($(this).find('span').text());
+
+                    selectList.slideUp(duration);
+                    selectHead.removeClass('on');
+                });
+
+            } else {
+                $(this).removeClass('on');
+                selectList.slideUp(duration);
+            }
+        });
+    });
+});
+
+
+
+// Проверяем, можно ли использовать Webp формат
+function canUseWebp() {
+    // Создаем элемент canvas
+    let elem = document.createElement('canvas');
+    // Приводим элемент к булеву типу
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // Создаем изображение в формате webp, возвращаем индекс искомого элемента и сразу же проверяем его
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+    // Иначе Webp не используем
+    return false;
+}
+
+window.onload = function () {
+    // Получаем все элементы с дата-атрибутом data-bg
+    let images = document.querySelectorAll('[data-bg]');
+    // Проходимся по каждому
+    for (let i = 0; i < images.length; i++) {
+        // Получаем значение каждого дата-атрибута
+        let image = images[i].getAttribute('data-bg');
+        // Каждому найденному элементу задаем свойство background-image с изображение формата jpg
+        images[i].style.backgroundImage = 'url(' + image + ')';
+    }
+
+    // Проверяем, является ли браузер посетителя сайта Firefox и получаем его версию
+    let isitFirefox = window.navigator.userAgent.match(/Firefox\/([0-9]+)\./);
+    let firefoxVer = isitFirefox ? parseInt(isitFirefox[1]) : 0;
+
+    // Если есть поддержка Webp или браузер Firefox версии больше или равно 65
+    if (canUseWebp() || firefoxVer >= 65) {
+        // Делаем все то же самое что и для jpg, но уже для изображений формата Webp
+        let imagesWebp = document.querySelectorAll('[data-bg-webp]');
+        for (let i = 0; i < imagesWebp.length; i++) {
+            let imageWebp = imagesWebp[i].getAttribute('data-bg-webp');
+            imagesWebp[i].style.backgroundImage = 'url(' + imageWebp + ')';
+        }
+    }
+};
